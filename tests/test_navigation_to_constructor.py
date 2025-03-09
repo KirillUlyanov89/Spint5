3b3f2e9e9f0d8e515dd6c4af3211b7213a453e04
KirillUlyanov89 <ulyanov.k.mgn@gmail.com>
2025-03-09 15:42:18 +0300

    Update test_navigation_to_constructor.py

--- tests/test_navigation_to_constructor.py
+++ tests/test_navigation_to_constructor.py
@@ -1,23 +1,8 @@
 import pytest
-import chromedriver_autoinstaller
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from src.locators import WebsiteLocators
 import src.data as data
-
-# Автоматическая установка ChromeDriver
-chromedriver_autoinstaller.install()
-
-
-@pytest.fixture
-def driver_chrome():
-    service = Service()
-    driver = webdriver.Chrome(service=service)
-    yield driver
-    driver.quit()
-
 
 class TestConstructorNavigation:
 
