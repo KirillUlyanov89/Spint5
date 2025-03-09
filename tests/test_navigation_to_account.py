07fda9fbaca487f17d85b3f39b8fb7bb576c730f
KirillUlyanov89 <ulyanov.k.mgn@gmail.com>
2025-03-09 15:40:56 +0300

    Update test_navigation_to_account.py

--- tests/test_navigation_to_account.py
+++ tests/test_navigation_to_account.py
@@ -1,21 +1,8 @@
 import pytest
-import chromedriver_autoinstaller
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from src.locators import WebsiteLocators
 import src.data as data
-
-# Установка ChromeDriver автоматически
-chromedriver_autoinstaller.install()
-
-@pytest.fixture
-def driver_chrome():
-    service = Service()
-    driver = webdriver.Chrome(service=service)
-    yield driver
-    driver.quit()
 
 class TestAccountNavigation:
 
