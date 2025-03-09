44baa1e28b6982cf2fb40cc2a78e7d0254a72cdf
KirillUlyanov89 <ulyanov.k.mgn@gmail.com>
2025-03-09 15:28:13 +0300

    Update test_navigation_to_constructor.py

--- tests/test_navigation_to_constructor.py
+++ tests/test_navigation_to_constructor.py
@@ -1,31 +1,58 @@
-
+import pytest
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from src.locators import WebsiteLocators
 import src.data as data
 
+# Автоматическая установка ChromeDriver
+chromedriver_autoinstaller.install()
+
+
+@pytest.fixture
+def driver_chrome():
+    service = Service()
+    driver = webdriver.Chrome(service=service)
+    yield driver
+    driver.quit()
+
+
 class TestConstructorNavigation:
+
+    def login(self, driver):
+        wait = WebDriverWait(driver, 60)
+        # Входим в личный кабинет пользователя
+        email_input = wait.until(EC.presence_of_element_located(WebsiteLocators.EMAIL_INPUT_FORM))
+        email_input.send_keys(data.test_user_login)
+
+        password_input = wait.until(EC.presence_of_element_located(WebsiteLocators.PASSWORD_INPUT_FORM))
+        password_input.send_keys(data.test_user_password)
+
+        login_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.LOGIN_BUTTON_FORM))
+        login_button.click()
+
+        # Ожидаем переход на страницу личного кабинета
+        wait.until(EC.url_to_be(data.main_page_url))
+
+    def navigate_to_account(self, driver):
+        wait = WebDriverWait(driver, 60)
+        account_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.ACCOUNT_BUTTON))
+        account_button.click()
+
+        # Ожидаем перехода на страницу личного кабинета
+        wait.until(EC.url_to_be(data.profile_page_url))
 
     def test_navigation_from_account_to_constructor_via_constructor_button(self, driver_chrome):
         driver = driver_chrome
         driver.get(data.login_page_url)
-        wait = WebDriverWait(driver, 60)
 
-        # Входим в личный кабинет пользователя
-        email_input = wait.until(EC.presence_of_element_located(WebsiteLocators.EMAIL_INPUT_FORM))
-        email_input.send_keys(data.test_user_login)
-        password_input = wait.until(EC.presence_of_element_located(WebsiteLocators.PASSWORD_INPUT_FORM))
-        password_input.send_keys(data.test_user_password)
-        login_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.LOGIN_BUTTON_FORM))
-        login_button.click()
-
-        # Переходим на страницу личного кабинета
-        wait.until(EC.url_to_be(data.main_page_url))
-        account_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.ACCOUNT_BUTTON))
-        account_button.click()
-        wait.until(EC.url_to_be(data.profile_page_url))
+        self.login(driver)
+        self.navigate_to_account(driver)
 
         # Кликаем кнопку "Конструктор"
+        wait = WebDriverWait(driver, 60)
         constructor_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.CONSTRUCTOR_BUTTON))
         constructor_button.click()
 
@@ -37,23 +64,12 @@ class TestConstructorNavigation:
     def test_navigation_from_account_to_constructor_via_logo(self, driver_chrome):
         driver = driver_chrome
         driver.get(data.login_page_url)
-        wait = WebDriverWait(driver, 60)
 
-        # Входим в личный кабинет пользователя
-        email_input = wait.until(EC.presence_of_element_located(WebsiteLocators.EMAIL_INPUT_FORM))
-        email_input.send_keys(data.test_user_login)
-        password_input = wait.until(EC.presence_of_element_located(WebsiteLocators.PASSWORD_INPUT_FORM))
-        password_input.send_keys(data.test_user_password)
-        login_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.LOGIN_BUTTON_FORM))
-        login_button.click()
-
-        # Переходим на страницу личного кабинета
-        wait.until(EC.url_to_be(data.main_page_url))
-        account_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.ACCOUNT_BUTTON))
-        account_button.click()
-        wait.until(EC.url_to_be(data.profile_page_url))
+        self.login(driver)
+        self.navigate_to_account(driver)
 
         # Кликаем на логотип Stellar Burgers
+        wait = WebDriverWait(driver, 60)
         logo_button = wait.until(EC.element_to_be_clickable(WebsiteLocators.SERVICE_LOGO_BUTTON))
         logo_button.click()
 
